4bcf0a6a9fd78e753177b96a4a406fbfa5344adc
ML <lepecq@hotmail.com>
2025-06-04 12:00:33 +0200

    add neovim installation

--- install.cmd
+++ install.cmd
@@ -5,3 +5,6 @@ winget install --id=wez.wezterm  -e
 
 echo "jet brains mono nerd font installation ..."
 winget install --id=DEVCOM.JetBrainsMonoNerdFont  -e
+
+echo "neovim installation ..."
+winget install --id=Neovim.Neovim  -e
